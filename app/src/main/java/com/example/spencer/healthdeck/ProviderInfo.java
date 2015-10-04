11c9ee32bf06486ee99f774826fccc4d95c84128
Spencer <spencerkthompson@gmail.com>
2015-10-03 21:23:51 -0700

    userMenu and ProviderInfo skeletons made

---
+++ app/src/main/java/com/example/spencer/healthdeck/ProviderInfo.java
@@ -0,0 +1,43 @@
+package com.example.spencer.healthdeck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+    /*
+    this class will take in a provider object through its message on the intent request.
+    It will then display all the providers information in an orderly fasion, and possibly
+    allow for contact to be made with doctor.  
+     */
+
+public class ProviderInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_provider_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
