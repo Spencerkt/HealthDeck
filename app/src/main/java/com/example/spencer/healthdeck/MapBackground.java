7986684300664fbcba2f297708cacb9f1a9667a7
Spencer <spencerkthompson@gmail.com>
2015-10-04 15:03:47 -0700

    map working

--- app/src/main/java/com/example/spencer/healthdeck/MapBackground.java
+++ app/src/main/java/com/example/spencer/healthdeck/MapBackground.java
@@ -1,8 +1,8 @@
 package com.example.spencer.healthdeck;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -11,13 +11,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class MapBackground extends FragmentActivity implements GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     DataFarm data = new DataFarm();
-    ArrayList<Provider> counselors, family, physical;
+    ArrayList<Provider> counselors, family, emergency;
+    ArrayList<Marker> markers;
+    boolean fam = false, couns = false, emer = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,7 @@ public class MapBackground extends FragmentActivity implements GoogleMap.OnInfoW
         super.onResume();
         setUpMapIfNeeded();
     }
+
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -56,19 +59,29 @@ public class MapBackground extends FragmentActivity implements GoogleMap.OnInfoW
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                setLocations( getButtonChoice() );  // change to setLocations();
+                setDefualt();
+                markers = makeMarkers( getCurrentDisplay() );
             }
         }
     }
 
+    private void setDefualt(){ fam = true; }
+
     private void fillArrays(){
         counselors = data.getCounselors();
         family = data.getFamily();
-        physical = data.getPT();
+        emergency = data.getER();
     }
 
-    private ArrayList<Provider> getButtonChoice(){
-        return family;
+    private ArrayList<Provider> getCurrentDisplay(){
+        if ( fam )
+            return family;
+        if ( couns )
+            return family;
+        if ( emer )
+            return emergency;
+        else
+            return family;              // Do I want to do that?
     }
 
     // Not used
@@ -76,13 +89,18 @@ public class MapBackground extends FragmentActivity implements GoogleMap.OnInfoW
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-    private void setLocations(ArrayList<Provider> currentData) {
-        for ( int i = 0; i < currentData.size(); i++){    // uncomment to run real program.
-            Provider current = currentData.get(i);
-            mMap.addMarker(new MarkerOptions().position(new LatLng(current.lat, current.lon)).title(current.docName).snippet(current.practice));
+
+    private ArrayList<Marker> makeMarkers(ArrayList<Provider> input){
+        ArrayList<Marker> marks = new ArrayList<>();
+
+        for (int i = 0; i < input.size(); i++){
+            marks.add(mMap.addMarker(new MarkerOptions().position(new LatLng(input.get(i).lat,
+                    input.get(i).lon)).title(input.get(i).docName).snippet(input.get(i).practice)));
             mMap.setOnInfoWindowClickListener(this);
         }
+        return marks;
     }
+
 
     public void onInfoWindowClick(Marker marker) {
         Intent providerPage = new Intent(this, ProviderInfo.class);
@@ -93,5 +111,62 @@ public class MapBackground extends FragmentActivity implements GoogleMap.OnInfoW
         Intent profilePage = new Intent(this, UserMenu.class);
         startActivity(profilePage);
     }
+
+
+    public void redButton(View view){
+        if ( emer )
+            return;
+        if ( fam )
+            fam = false;
+            setToFalse(markers);
+        if ( couns )
+            couns = false;
+            setToFalse(markers);
+
+        reMap(emergency);
+    }
+
+    public void blueButton(View view){
+        if ( fam )
+            return;
+        if ( emer )
+            emer = false;
+        setToFalse(markers);
+        if ( couns )
+            couns = false;
+        setToFalse(markers);
+
+        reMap(family);
+    }
+
+    public void yellowButton(View view){
+        if ( couns )
+            return;
+        if ( fam )
+            fam = false;
+        setToFalse(markers);
+        if ( emer )
+            emer = false;
+        setToFalse(markers);
+
+        reMap(counselors);
+    }
+
+    private void setToFalse(ArrayList<Marker> currentMarks){
+        for (int i = 0; i < currentMarks.size(); i++){
+            currentMarks.get(i).setVisible(false);
+        }
+    }
+
+    private void reMap(ArrayList<Provider> currentMarks){
+        markers = new ArrayList<>();
+
+        for (int i = 0; i < currentMarks.size(); i++){
+            markers.add(mMap.addMarker(new MarkerOptions().position(new LatLng(currentMarks.get(i).lat,
+                    currentMarks.get(i).lon)).title(currentMarks.get(i).docName).snippet(currentMarks.get(i).practice)));
+            mMap.setOnInfoWindowClickListener(this);
+        }
+    }
+
 
 }
